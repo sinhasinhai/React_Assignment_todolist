6c14eba66e0d4678bbde0517a478d3082be74ed6
sinhasinhai <snehasinha348@gmail.com>
2020-10-04 14:49:49 +0530

    first commit1-04/10/2020

---
+++ src/ListItems.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './ListItems.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import FlipMove from 'react-flip-move';
+
+
+//funtional based component
+function ListItems(props){
+    const items = props.items;
+    const listItems = items.map(item =>
+   {
+       return <div className="list" key={item.key}>
+     <p>
+         <input type="text" id={item.key} value={item.text} onChange={(e)=>{
+             props.setUpdate(e.target.value,item.key)}}/>
+        <span>
+       
+        <FontAwesomeIcon className="faicons" onClick={() => {
+            props.deleteItem(item.key)
+        }} icon="trash" />
+        </span>
+     </p>
+     
+    </div>})
+    return <div>
+        {/* FlipMove for the animation will addding and removing */}
+        <FlipMove duration={400} easing="ease-in-out">
+        {listItems}
+        </FlipMove>
+    
+    </div>;
+  }
+
+  export default ListItems;
